2db27d74ea44f25de9c161aa53c2e1a464d99177
eetu-niittymaki <eetu945@gmail.com>
2024-09-05 19:07:07 +0300

    Several changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 /* eslint-disable default-case */
 import './App.css';
 import { useEffect, useState, useRef } from 'react';
+import ImageMapper from 'react-img-mapper';
+import * as maps from "./utils/mapObject.js"
 import GameboyJS from './dist/gameboy'; // Import the GameboyJS library
 
 let g
@@ -8,21 +10,13 @@ let g
 function App() {
   const soundEnableRef = useRef(null);   // Ref for the sound checkbox
   const gameboyInstance = useRef(null);  // Ref to store the Gameboy instance
-  const [zoom, setZoom] = useState("1")
+  const [zoom, setZoom] = useState("2")
   const [run, setRun] = useState(false)
   const [canvasWidth, setCanvasWidth] = useState(160)
   const [canvasHeight, setCanvasHeight] = useState(144)
+  const scrollKeys = false
 
-  const positionLeftSwitch = () => {
-    switch(zoom) {
-      case "1": return "15%"
-      case "2": return "5%"
-      case "3": return "-5%"
-    }
-  }
 
-  console.log(zoom)
-  
   useEffect(() => {
     // Configuration of the GameboyJS emulator
     const options = {
@@ -33,10 +27,8 @@ function App() {
       ]
     };
     const canvas = document.getElementById('canvas')
-    const c = canvas.getContext("2d")
-    const image = document.getElementById("gb-image")
     g = new GameboyJS.Gameboy(canvas, options);
-    c.drawImage(image, 0,0)
+
   }, [])
 
   useEffect(() => {
@@ -47,13 +39,25 @@ function App() {
     const position = canvas.getBoundingClientRect()
     setCanvasWidth(position.width)
     setCanvasHeight(position.height)
+    // Disable keyboard scrolling
+    document.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "ArrowLeft":
+        case "ArrowRight":
+        case "ArrowUp":
+        case "ArrowDown":
+          if (!scrollKeys) e.preventDefault();
+          break;
+        default: return true
+      }
+    })
   })
 
   const pauseGame = (pause) => {
     if (gameboyInstance.current) {
       gameboyInstance.current.pause(pause);
     }
-  };
+  }
 
   const handleChange = (event) => {
     const file = event.target.files[0]
@@ -65,93 +69,71 @@ function App() {
       <header className="App-header">
         <h1 className="headerTitle">ReactBoy</h1>
       </header>
-      <div id="container" className="App">
         <div className="canvasOuter">
-            <div className="canvasInner">
-              <img id="gb-image"
-                    className="gb-img"
-                    src={run ? "/gameboy-on.png" : "/gameboy-off.png"}
-                    width={canvasWidth * 3}
-                    height={canvasHeight * 3} 
-                    alt="Gameboy"
-                    style={{left: positionLeftSwitch()}}/>
-              <canvas id="canvas" 
-                      className="canvas"
-                      width="160"
-                      height="144">
-                    Your browser does not seem to support canvas.
-              </canvas>
-            </div>
+          <div className="canvasInner" id="canvasInner">
+          <ImageMapper src={run ? "/gameboy-on.png" : "/gameboy-off.png"}
+                        map={maps.MAP}
+                        width={canvasWidth * 2 * 0.90}
+                        height={canvasHeight * 3.35}
+                        alt="Gameboy"
+                        responsive={true}
+                        parentWidth={500}
+                        />
+            <canvas id="canvas"
+              className="canvas"
+              width="160"
+              height="144">
+              Your browser does not seem to support canvas.
+            </canvas>
           </div>
-        <p className="commands">
-          <button onClick={() => pauseGame(true)}>Pause</button>
-          <button onClick={() => pauseGame(false)}>Run</button>
-        </p>
-
-        <p className="commands">
-          <label>
-            <input
-              ref={soundEnableRef}
-              id="sound-enable"
-              type="checkbox"
-              defaultChecked={false}
-            /> 
-            Enable sound (experimental)
-          </label>
-        </p>
-
-        <p className="commands" >
-          <label>
-            Screen zoom
-            <select id="screen-zoom" 
-                    defaultValue="1" 
-                    onChange={(e) => setZoom(e.target.value)}>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
-          </label>
-        </p>
-
-        <p>
-          <span id="status"></span> <span id="game-name"></span>
-        </p>
-        <p id="error" className="hide"></p>
-
-        <div className="rom-section">
-          <div id="rom-file">
+        </div>
+        <div id="container" className="App" >
+          {/*<p className="commands">
+            <button onClick={() => pauseGame(true)}>Pause</button>
+            <button onClick={() => pauseGame(false)}>Run</button>
+          </p>*/}
+          <p className="commands">
             <label>
-              Choose a ROM file on your computer:
-              <input type="file" 
-                     id="file" 
-                     onChange={handleChange}
+              <input
+                ref={soundEnableRef}
+                id="sound-enable"
+                type="checkbox"
+                defaultChecked={false}
               />
+              Enable sound (experimental)
             </label>
-          </div>
-
-          <div id="rom-drop">
-            <div id="dropzone">
-              <p>Or drop a ROM file here</p>
+          </p>
+          <p>
+            <span id="status"></span> <span id="game-name"></span>
+          </p>
+          <p id="error" className="hide"></p>
+          <div className="rom-section">
+            <div id="rom-file">
+              <label>
+                Choose a ROM file on your computer:
+                <input type="file"
+                  id="file"
+                  onChange={handleChange}
+                />
+              </label>
             </div>
           </div>
+          <div id="keyboard-info" >
+            <p>Current keyboard mapping:</p>
+            <table>
+              <thead>
+                <tr><th>Gameboy pad</th><th>Keyboard mapping</th></tr>
+              </thead>
+              <tbody>
+                <tr><td>A</td><td>G</td></tr>
+                <tr><td>B</td><td>B</td></tr>
+                <tr><td>START</td><td>H | Enter</td></tr>
+                <tr><td>SELECT</td><td>N</td></tr>
+                <tr><td>Directional pad</td><td>Arrow keys</td></tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-
-        <div id="keyboard-info" >
-          <p>Current keyboard mapping:</p>
-          <table>
-            <thead>
-              <tr><th>Gameboy pad</th><th>Keyboard mapping</th></tr>
-            </thead>
-            <tbody>
-              <tr><td>A</td><td>G</td></tr>
-              <tr><td>B</td><td>B</td></tr>
-              <tr><td>START</td><td>H | Enter</td></tr>
-              <tr><td>SELECT</td><td>N</td></tr>
-              <tr><td>Directional pad</td><td>Arrow keys</td></tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
     </div>
   );
 }
---
+++ src/utils/mapObject.js
@@ -0,0 +1,13 @@
+export const MAP = {
+    name: "Lameboy",
+    areas: [
+      {id: "A", name: "A-button", shape: "circle", preFillColor: "rgba(255, 255, 255, 0.0)", strokeColor: "#c4bebb", coords: [1022, 1237, 80]},
+      {id: "B", name: "B-button", shape: "circle", preFillColor: "rgba(255, 255, 255, 0.0)", strokeColor: "#c4bebb", coords: [832, 1327, 80]},
+      {id: "UP", name: "UP", shape: "rect", preFillColor: "rgba(255, 255, 255, 0.0)", strokeColor: "#c4bebb", coords: [305, 1150, 190, 1245]},
+      {id: "DOWN", name: "DOWN", shape: "rect", preFillColor: "rgba(255, 255, 255, 0.0)", strokeColor: "#c4bebb", coords: [305, 1450, 190, 1360]},
+      {id: "LEFT", name: "LEFT", shape: "rect", preFillColor: "rgba(255, 255, 255, 0.0)", strokeColor: "#c4bebb", coords: [95, 1355, 190, 1242]},
+      {id: "RIGHT", name: "RIGHT", shape: "rect", preFillColor: "rgba(255, 255, 255, 0.0)", strokeColor: "#c4bebb", coords: [395, 1355, 305, 1242]},
+      {id: "SELECT", name: "SELECT", shape: "rect", preFillColor: "rgba(255, 255, 255, 0.0)", strokeColor: "#8d8a90", coords: [520, 1535, 360, 1680]},
+      {id: "START", name: "START", shape: "rect", preFillColor: "rgba(255, 255, 255, 0.0)", strokeColor: "#8d8a90", coords: [715, 1535, 555, 1680]},
+    ]
+}
